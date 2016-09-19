ab8df44e260c585dcc2ec73cafc9cba92dd38564
Philibert Dugas <philibert.dugas@gmail.com>
2016-09-19 17:58:19 -0400

    Working version with logs

--- motion.py
+++ motion.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+
 from gpiozero import MotionSensor
-from time import sleep
+from time import sleep, strftime
+import sys
 
 pir = MotionSensor(4)
 
 while True:
+  current_time = strftime("%Y-%m-%d %H:%M:%S")
   if pir.is_active:
-    print("Active")
+    sys.stdout.write("[%s] Active\n" % current_time)
   else:
-    print("Nope")
-  sleep(3)
+    sys.stdout.write("[%s] Inactive\n" % current_time)
+  sys.stdout.flush()
+  sleep(30)
--- start_motion.sh
+++ start_motion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PATH=/usr/local/bin:/bin/:/usr/bin:/sbin:/usr/sbin
-python3 /home/motion.py &> /dev/null &
+python3 /home/motion.py >> /home/motion.log
