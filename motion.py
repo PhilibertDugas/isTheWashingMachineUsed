5670f9767d15983f3623265546810449d0022f97
Philibert Dugas <philibert.dugas@gmail.com>
2016-10-02 09:13:21 -0400

    adding result

--- motion.py
+++ motion.py
@@ -9,8 +9,8 @@ pir = MotionSensor(4)
 while True:
   current_time = strftime("%Y-%m-%d %H:%M:%S")
   if pir.is_active:
-    sys.stdout.write("[%s] Active\n" % current_time)
+    sys.stdout.write("[%s],Active\n" % current_time)
   else:
-    sys.stdout.write("[%s] Inactive\n" % current_time)
+    sys.stdout.write("[%s],Inactive\n" % current_time)
   sys.stdout.flush()
-  sleep(30)
+  sleep(10)
